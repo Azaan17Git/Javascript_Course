c8871563c4c7c7ae50119a82d5eacf2d158e87cd
Azaan17Git <Azaan.official17@gmail.com>
2025-07-29 21:51:36 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+let name = "Azaan Manzoor"
+let age = 23
+let height = 5.9
+let salary = 222230239493834834949073974389587588518935984958749574589157554854895494834
+let isLoggedin = false
+
+console.log(name)
+console.log(age)
+console.log(height)
+console.log(salary)
+console.log(isLoggedin)
+
+console.log(typeof null)
