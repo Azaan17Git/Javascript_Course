68860431da6e330eaaf03cfbde4dfd01f11e72c3
Azaan17Git <Azaan.official17@gmail.com>
2025-07-29 21:04:09 +0000

    data config done

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 78625
+let accountEmail = "Azaan.official17@gmail.com"
+var accountPassword = "Azaan17"
+accountCity = "Lahore"
+
+// accountId = 56565 not allowed js.
+
+accountEmail = "Azaan3709@gmail.com"
+accountPassword = "Azaan56"
+accountCity = "Islamabad"
+
+// So, we can change the variables not constant.
+// Not prefer to use var in js because issue in block scope and functional scope.
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
