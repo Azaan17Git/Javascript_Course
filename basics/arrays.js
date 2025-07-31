d2ff9528d959d49b699f4d5843d38954964b8b68
Azaan17Git <Azaan.official17@gmail.com>
2025-07-31 23:39:26 +0000

    container config done

--- basics/arrays.js
+++ basics/arrays.js
@@ -1,11 +1,10 @@
-const myarr = [0,1,2,3,4,5]
-const fvrtCricketers = ["Babar", "Virat", "Root", "Afridi", "Shahdab"]
+const arr = [0,1,2,3,4,5]
 // console.log(arr);
 // console.log(arr[3]);
 // console.log(fvrtCricketers);
 
-const arr = new Array(1,2,3,4,5)
-myarr.push(6)
+const myarr = new Array(1,2,3,4,5)
+// myarr.push(6)
 // myarr.pop()
 // myarr.unshift(0)
 // myarr.shift()
@@ -26,3 +25,26 @@ myarr.push(6)
 // console.log("C ", myarr);
 // console.log(myn2);
 
+const fvrtBatsmen = ["Babar", "Virat", "Root", "Rohit", "AB De Villiers"]
+// console.log(fvrtBatsmen);
+const fvrtBowlers = ["Bumrah", "Starc", "Shaheen", "Adil Rashid", "Wood"]
+// fvrtBatsmen.push(fvrtBowlers)
+const fvrtCricketers = fvrtBatsmen.concat(fvrtBowlers)
+console.log(fvrtCricketers);
+
+// const fvrtCricketers = [...fvrtBatsmen,...fvrtBowlers]
+// console.log(fvrtCricketers); another way to merge the two arrays
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Azaan"))
+console.log(Array.from("Azaan"))
+console.log(Array.from({name: "Azaan"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
