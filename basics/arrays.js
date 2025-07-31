82e58f8210d5e4b499cd1c810d3e5d349d80c5d2
Azaan17Git <Azaan.official17@gmail.com>
2025-07-31 23:11:19 +0000

    container congfig done

---
+++ basics/arrays.js
@@ -0,0 +1,28 @@
+const myarr = [0,1,2,3,4,5]
+const fvrtCricketers = ["Babar", "Virat", "Root", "Afridi", "Shahdab"]
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(fvrtCricketers);
+
+const arr = new Array(1,2,3,4,5)
+myarr.push(6)
+// myarr.pop()
+// myarr.unshift(0)
+// myarr.shift()
+// console.log(myarr.includes(7));
+// console.log(myarr.indexOf(5));
+
+// const newarr = new myarr.join()
+// console.log(myarr);
+// console.log(newarr);
+
+// console.log("A ", myarr);
+// const myn1 = myarr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myarr);
+
+// const myn2 = myarr.splice(1, 3)
+// console.log("C ", myarr);
+// console.log(myn2);
+
