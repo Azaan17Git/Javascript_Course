c77c61c20422d14909b986d7e5696eec703948e5
Azaan17Git <Azaan.official17@gmail.com>
2025-08-06 21:27:37 +0000

    container config done

---
+++ control_flow/switch.js
@@ -0,0 +1,56 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }  syntax 
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("Januray");
+//         break;
+//     case 2:
+//         console.log("Feburary");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     default:
+//         console.log("default case matched.");
+//         break;
+// }
+
+const month = "Apr"
+
+switch (month) {
+    case "Jan":
+        console.log("Januray");
+        break;
+    case "Feb":
+        console.log("Feburary");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case matched.");
+        break;
+}
