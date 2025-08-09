b4573f68cd0713731ae9e8db00f7dab9979d864a
Azaan17Git <Azaan.official17@gmail.com>
2025-08-09 04:45:23 +0000

    container config done

---
+++ iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is: ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = ["Shaheen", "Bumrah", "Adil Rashid"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`Name of my fvrt bowler is:${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 1
+
+do {
+    console.log(`Score is:${score}`);
+    score++
+} while (score <=10);
